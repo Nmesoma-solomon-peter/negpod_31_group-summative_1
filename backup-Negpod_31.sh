b39e7db3fa86ee0dbc920af55cb48cec0b1c1355
nmesoma <nmesomasolo2809@gmail.com>
2024-02-16 22:24:28 +0200

    updated backup with new cred...

--- backup-Negpod_31.sh
+++ backup-Negpod_31.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Replace the following variables with your specific details
-remote_host="2f05c1f8800b.3be8ebfc.alu-cod.online"
-remote_username="2f05c1f8800b"
-remote_password="d4a1d225d0abda9549d8"
+remote_host="64293e56bc62.3a2627c1.alu-cod.online"
+remote_username="64293e56bc62"
+remote_password="328d3b338a4ced526c9a"
 remote_location="/summative/1023-2024j"
 local_directory="negpod_31-q1"
 
