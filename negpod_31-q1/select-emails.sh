1433e185602f6688b702b6d640921baddccf480e
tracy8 <tracymurenzi@gmail.com>
2024-02-25 20:54:58 +0200

    emails

--- negpod_31-q1/select-emails.sh
+++ negpod_31-q1/select-emails.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #This script selects all student emails from main.sh file and store them in a file named student-emails.txt
+#You can run this script by typing ./select-emails.sh in the terminal
 
 students_file="students-list_1023.txt"
 output_file="student-emails.txt"
